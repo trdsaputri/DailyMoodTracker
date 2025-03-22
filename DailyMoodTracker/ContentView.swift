d4354c73ae21b526c83eda1497c7b4fb7673f241
trdsaputri <88604546+trdsaputri@users.noreply.github.com>
2025-03-22 10:08:12 +0700

    Initial Commit

---
+++ DailyMoodTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyMoodTracker
+//
+//  Created by Theresia Saputri on 22/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyMoodTracker/DailyMoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMoodTrackerApp.swift
+//  DailyMoodTracker
+//
+//  Created by Theresia Saputri on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyMoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
